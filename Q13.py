0a6a6441069be8659a3a6860799d4aaf073cd340
Minyoung Kim <91856370+Skyminn@users.noreply.github.com>
2022-01-26 15:10:06 +0900

    chapter4

---
+++ Q13.py
@@ -0,0 +1,20 @@
+#연습문제 13-1 회문찾기
+def palindrome(s):
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        if s[i].isalpha() == False:
+            i += 1
+        elif s[j].isalpha() == False:
+            j -= 1
+        elif s[i].lower() != s[j].lower():
+            return False
+        else:
+            i += 1
+            j -= 1
+    return True
+
+print(palindrome("Wow"))
+print(palindrome("Madam, I'm Adam."))
+
+
